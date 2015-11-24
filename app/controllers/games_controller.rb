76098397253f95f1ad37f9fd486aa84566fb183c
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-24 13:05:07 -0800

    basic form for new games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,21 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def new
+    @game = Game.new
+    @url_path = games_path
+  end
+
+  def create
+    Game.create(game_params[:game])
+    redirect_to games_path
+  end
+
+
+  private
+
+  def game_params
+    params.permit(game:[:name, :genre, :time_to_play])
+  end
 end
