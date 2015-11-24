5773af7f182ef5f325d0e9b29d53a3998c8b0ef1
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-24 15:32:22 -0800

    basic CRUD for Meetup

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -6,4 +6,35 @@ class MeetupsController < ApplicationController
   def show
     @meetup = Meetup.find(params[:id])
   end
+
+  def new
+    @meetup = Meetup.new
+    @url_path = meetups_path
+  end
+
+  def create
+    Meetup.create(meetup_params[:meetup])
+    redirect_to meetups_path
+  end
+
+  def edit
+    @meetup = Meetup.find(params[:id])
+    @url_path = meetup_path
+  end
+
+  def update
+    Meetup.update(params[:id], meetup_params[:meetup])
+    redirect_to meetups_path
+  end
+
+  def destroy
+    Meetup.destroy(params[:id])
+    redirect_to meetups_path
+  end
+
+  private
+
+  def meetup_params
+    params.permit(meetup:[:location, :date, :duration])
+  end
 end
