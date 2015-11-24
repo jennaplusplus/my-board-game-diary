3566bb503a96c312d0d62fd4023e80dcb0654a2c
Jenna Nichols <jennaplusplus@gmail.com>
2015-11-24 12:46:04 -0800

    basic game index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Game info
+# t.string   "name"
+# t.float    "time_to_play"
+# t.string   "genre"
+# t.integer  "min_players"
+# t.integer  "max_players"
+
+games = [
+  { name: "The Settlers of Catan",  time_to_play: 1.5,  genre: "strategy",    min_players: 3, max_players: 4 },
+  { name: "Pandemic",               time_to_play: 1.5,  genre: "co-op",       min_players: 2, max_players: 4 },
+  { name: "Battlestar Galactica",   time_to_play: 4,    genre: "thematic",    min_players: 3, max_players: 6 },
+  { name: "Power Grid",             time_to_play: 2.5,  genre: "strategy",    min_players: 2, max_players: 6 },
+]
+
+games.each do |game|
+  Game.create(game)
+end
