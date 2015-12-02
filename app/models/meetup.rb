8aaed00a03bd6a0bedbe56adc37b20e6331f3e51
Jenna Nichols <jennaplusplus@gmail.com>
2015-12-01 20:45:19 -0800

    validations for meetup

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,4 +1,8 @@
 class Meetup < ActiveRecord::Base
   has_many :events
   has_and_belongs_to_many :players
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :date, presence: true
 end
