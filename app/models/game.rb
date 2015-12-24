1ff8d845f8f16b3dccf6fb2d94accfb653ed0b11
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-24 15:22:37 -0800

    added avg_rating method, and added to show page

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,19 @@
 class Game < ActiveRecord::Base
   has_many :events
   has_and_belongs_to_many :players
+
+  def avg_rating
+    avg = []
+    self.events.each do |event|
+      if !event.rating.nil?
+        avg << event.rating
+      end
+    end
+    if avg.length > 0
+      return avg.inject(0){|r, e| r + e}
+    else
+      return nil
+    end
+  end
+
 end
