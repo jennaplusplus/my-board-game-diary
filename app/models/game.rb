7d0f76fa871f03483330ae449a594609b99badc2
Riley Spicer <riley.r.spicer@gmail.com>
2015-12-24 15:35:11 -0800

    changed avg_rating to floats

--- app/models/game.rb
+++ app/models/game.rb
@@ -10,7 +10,7 @@ class Game < ActiveRecord::Base
       end
     end
     if avg.length > 0
-      return avg.inject(0){|r, e| r + e} / avg.length
+      return avg.inject(0){|r, e| r + e}.to_f / avg.length.to_f
     else
       return nil
     end
