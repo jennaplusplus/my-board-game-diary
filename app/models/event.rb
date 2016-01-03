39cb82c9341d94f61ab51a0d4e0e5ac3961a873f
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-02 17:52:26 -0800

    syntax error fixed

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ActiveRecord::Base
   belongs_to :game
   has_and_belongs_to_many :players
 
-  validates :rating, presence: true, numericality: { :less_than_or_equal_to 10 }
+  validates :rating, presence: true, numericality: { less_than_or_equal_to: 10 }
 end
