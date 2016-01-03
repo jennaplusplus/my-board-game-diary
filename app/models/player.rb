8df6881010d403c447aa8ba4d7295fec7b1e846b
Rebecca Tolmach <rmtolmach@gmail.com>
2016-01-02 17:49:18 -0800

    validations for player and event models

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,10 @@ class Player < ActiveRecord::Base
   has_and_belongs_to_many :meetups
   has_and_belongs_to_many :events
   has_and_belongs_to_many :games
+
+  validates :name, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :age, presence: true, numericality: { greater_than: 0 }
+
 end
